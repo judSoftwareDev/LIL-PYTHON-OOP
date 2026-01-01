471b897b3995fcc875e910a76fe82469d9d8bd6d
pinzap_ <151607984+judSoftwareDev@users.noreply.github.com>
2026-01-01 23:06:35 +0800

    Add files via upload

---
+++ GETTING-STARTED/challenge_creating_objects.py
@@ -0,0 +1,18 @@
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+     
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+     
+if __name__ == "__main__":
+    card1 = Card("Ace", "Spades")
+    card2 = Card("Queen", "Hearts")
+    card3 = Card("Jack","Clubs")
+    card4 = Card("Three","Diamonds")
+    
+    print(card1)
+    print(card2)
+    print(card3)
+    print(card4)
